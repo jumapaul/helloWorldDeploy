c2ca8b5e8301f1296b708a8f102774e2d66563d3
jumapaul <pauljuma736@gmail.com>
2026-07-17 16:00:11 +0300

    Sealing secrets using bitnami

--- hello-world/src/main/java/com/helloworld/HelloWorldController.java
+++ hello-world/src/main/java/com/helloworld/HelloWorldController.java
@@ -6,12 +6,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/helloWorld")
 public class HelloWorldController {
+    private final HelloWorldRepository helloWorldRepository;
+
+    public HelloWorldController(HelloWorldRepository repository) {
+        this.helloWorldRepository = repository;
+    }
 
     @GetMapping
     public ResponseEntity<String> helloWorld() {
         return ResponseEntity.ok("Hello world");
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Points>> getAll() {
+        return ResponseEntity.ok(helloWorldRepository.findAll());
     }
 }
---
+++ hello-world/src/main/java/com/helloworld/HelloWorldRepository.java
@@ -0,0 +1,6 @@
+package com.helloworld;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HelloWorldRepository extends JpaRepository<Points, Integer> {
+}
