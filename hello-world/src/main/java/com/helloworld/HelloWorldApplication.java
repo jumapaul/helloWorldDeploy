086ae92567ac23013e8bfd200215965ae6c4a0c4
jumapaul <pauljuma736@gmail.com>
2026-07-20 13:01:41 +0300

    Minor update

--- hello-world/src/main/java/com/helloworld/HelloWorldApplication.java
+++ hello-world/src/main/java/com/helloworld/HelloWorldApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelloWorldApplication {
-
-    // Spring application
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldApplication.class, args);
     }
