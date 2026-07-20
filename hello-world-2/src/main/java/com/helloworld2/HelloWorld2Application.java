93ebc338fdaa62834a5025b6b20ad17e5a912d84
jumapaul <pauljuma736@gmail.com>
2026-07-20 12:46:14 +0300

    Minor update

--- hello-world-2/src/main/java/com/helloworld2/HelloWorld2Application.java
+++ hello-world-2/src/main/java/com/helloworld2/HelloWorld2Application.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloWorld2Application {
 
-    // Spring application
     public static void main(String[] args) {
         SpringApplication.run(HelloWorld2Application.class, args);
     }
--- hello-world/src/main/java/com/helloworld/HelloWorldApplication.java
+++ hello-world/src/main/java/com/helloworld/HelloWorldApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloWorldApplication {
 
-    // Spring application
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldApplication.class, args);
     }
